618888716c201a3d599bdb7c3f0766c1e08fd8d7
Lewis Meyers <meyerslewis193@gmail.com>
2023-08-01 20:25:18 -0400

    splash page 90% done

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
 import Image from "next/image";
+import { Julius_Sans_One, Lexend_Zetta } from "next/font/google";
+
+const julius = Julius_Sans_One({ weight: "400", subsets: ["latin"] });
+const lexend = Lexend_Zetta({ weight: "400", subsets: ["latin"] });
 
 const founders = [
   {
     name: "Lewis Meyers",
     imageSrc: "/x.PNG",
+    twitter: "https://twitter.com/DevRelLewis",
+    github: "https://github.com/DevRelLewis",
   },
   {
     name: "Kasi Reeves",
     imageSrc: "/g.PNG",
+    twitter: "https://twitter.com/RobotProxyWar",
+    github: "https://github.com/Kasirocswell",
   },
 ];
 
@@ -18,13 +26,17 @@ export default function Home() {
         className="flex flex-col h-screen w-screen bg-cover bg-center"
         style={{ backgroundImage: "url(/cdbg.jpg)" }}
       >
-        <button className="w-[75px] h-[50px] bg-[#a212b5] rounded-xl ml-[75%] mt-2">
+        <button
+          className={`w-[75px] h-[50px] bg-[#a212b5] rounded-xl ml-[75%] mt-2 2xl:ml-[90%] 2xl:mt8 ${julius.className}`}
+        >
           LOGIN
         </button>
-        <div className="flex flex-col mx-auto my-auto text-6xl">
+        <div
+          className={`flex flex-col mx-auto my-auto text-6xl ${lexend.className} 2xl:text-[125px]`}
+        >
           <p>An</p>
           <p>Immersive</p>
-          <p>MUD</p>
+          <p className="text-[#a212b5]">MUD</p>
           <p>Experience</p>
         </div>
       </section>
@@ -32,17 +44,19 @@ export default function Home() {
         className="flex flex-col h-screen w-screen bg-cover bg-center"
         style={{ backgroundImage: "url(/cdbg2.jpg)" }}
       >
-        <p className="text-xl mx-auto my-auto text-center">
-          Dive into the "Celestial Deep", an immersive MUD that propels you into
-          the vast expanse of the cosmos. Navigate through alien worlds, engage
-          in epic starship battles, and unravel the enigmas of the universe.
-          Encounter a myriad of alien species, each with unique cultures and
-          narratives. Choose to trade, negotiate, or engage in combat - the
-          choice is yours. Customize your starship, venture into unexplored
-          territories, and etch your name in the stars. Whether you're an
-          explorer, a trader, or a warrior, your interstellar adventure awaits.
-          Are you ready to plunge into the celestial deep? Join us in the cosmos
-          today!
+        <p
+          className={`text-xl mx-auto my-auto text-center 2xl:w-[80%] 2xl:text-4xl ${lexend.className} `}
+        >
+          Dive into the <span className="text-[#a212b5]">"Celestial Deep"</span>
+          , an immersive MUD that propels you into the vast expanse of the
+          cosmos. Navigate through alien worlds, engage in epic starship
+          battles, and unravel the enigmas of the universe. Encounter a myriad
+          of alien species, each with unique cultures and narratives. Choose to
+          trade, negotiate, or engage in combat - the choice is yours. Customize
+          your starship, venture into unexplored territories, and etch your name
+          in the stars. Whether you're an explorer, a trader, or a warrior, your
+          interstellar adventure awaits. Are you ready to plunge into the
+          celestial deep? Join us in the cosmos today!
         </p>
       </section>
       <section
@@ -51,7 +65,10 @@ export default function Home() {
       >
         <div className="flex mx-auto my-auto space-x-10">
           {founders.map((founder, index) => (
-            <div key={index} className="flex flex-col items-center">
+            <div
+              key={index}
+              className="flex flex-col items-center 2xl:w-[400px] 2xl:h-[600px]"
+            >
               <Image
                 src={founder.imageSrc}
                 alt={founder.name}
@@ -59,13 +76,13 @@ export default function Home() {
                 height={100}
                 className="rounded-full"
               />
-              <h2 className="text-white">{founder.name}</h2>
-              <p className="text-white">founder</p>
+              <h2 className="text-white 2xl:text-4xl">{founder.name}</h2>
+              <p className="text-white 2xl:text-2xl">founder</p>
               <div className="flex space-x-2">
-                <a href="https://twitter.com/username">
+                <a href={founder.twitter}>
                   <img src="/x.PNG" alt="Twitter" />
                 </a>
-                <a href="https://github.com/username">
+                <a href={founder.github}>
                   <img src="/g.PNG" alt="Github" />
                 </a>
               </div>
